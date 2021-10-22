770bc73899de6938563285090f4f9dd6a606fe76
ian balijawa <ianbalijawa16@gmail.com>
2021-10-22 18:01:53 +0300

    remove faq from pricing

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import Header from '../components/Header/light';
-import FAQ from '../components/Faqs/SingleCol';
+// import FAQ from '../components/Faqs/SingleCol';
 import Product from '../components/Products/Product';
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -14,7 +14,7 @@ export default () => {
 		<>
 			<Header hiddenLink='download' />
 			<Product />
-			<FAQ />
+			{/* <FAQ /> */}
 		</>
 	);
 };
